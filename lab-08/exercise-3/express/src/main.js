4fd08947f4b87e2cdeb84e98407533387c35e3af
Ciszkoo <pawel.olszko.p@gmail.com>
2023-05-13 21:59:55 +0200

    lab 8, ex 3

---
+++ lab-08/exercise-3/express/src/main.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { createClient } from "redis";
+import pg from "pg";
+
+const { Client } = pg;
+
+const redis = createClient({
+  socket: { port: 6379, host: "l8ex3-redis" },
+});
+
+redis.on("error", (err) => console.error("Redis client error", err));
+
+const postgres = new Client({
+  host: "l8ex3-postgres",
+  port: 5432,
+  user: "postgres",
+  password: "postgres",
+});
+
+const app = express();
+
+app.get("/hello", async (req, res) => {
+  return res.status(200).json({ message: "Hello" });
+});
+
+app.get("/redis", async (req, res) => {
+  await redis.connect();
+
+  res.status(200).json({ message: await redis.ping() });
+
+  await redis.quit();
+});
+
+app.get("/postgres/:id", async (req, res) => {
+  await postgres.connect();
+
+  const queryResult = await postgres.query("SELECT $1::text as message", [
+    "Hello world!",
+  ]);
+  res.status(200).json({ message: queryResult.rows[0].message });
+
+  await postgres.end();
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
